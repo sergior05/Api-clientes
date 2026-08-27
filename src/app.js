6e18c777052c6ecb30bfc20a00b75e70495e7b9e
sergio rodriguez <sergio.rodriguezrojas07@gmail.com>
2026-08-27 00:07:46 -0500

    prueba1

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors'); // 1. Importa el paquete cors
 const clienteRoutes = require('./routes/clienteRoutes');
 
 const app = express();
 
+app.use(cors()); // 2. Actívalo para todas las peticiones
 app.use(express.json());
 
 app.get('/', (req, res) => {
